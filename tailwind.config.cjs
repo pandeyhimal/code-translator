50fe428af7c06e38ac8e48dd0b133fb9e1bc709c
itsm22051@gmail.com <himalpandey2002@gmail.com>
2026-07-18 02:44:43 +0545

    dependencies

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
